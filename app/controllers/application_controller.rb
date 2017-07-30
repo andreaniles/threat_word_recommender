f7e03b2210a627f4ce2ce5de90d28a25b35a8b86
Richard Niles <richkniles@Richards-MacBook-Pro-3.local>
2017-07-30 16:46:38 -0700

    put split back

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,6 +22,6 @@ class ApplicationController < ActionController::Base
       @answers.each { |word, rating| csv << [word, rating] }
     end
 
-    @result = `Rscript /app/bin/rscript/threatwords.r #{sess_token}`
+    @result = `Rscript /app/bin/rscript/threatwords.r #{sess_token}`.split(' ')[1]
   end
 end
