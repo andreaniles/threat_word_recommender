a50742e83c4070261cbe561e87d21a523f34c66a
Andrea Niles <andreaniles@MacBook-Pro.local>
2017-08-01 21:20:39 -0700

    Add more rscript and csv of words

--- bin/rscript/threatwords.r
+++ bin/rscript/threatwords.r
@@ -1,6 +1,25 @@
 #!/usr/bin/env Rscript
+
 sess_token = commandArgs(trailingOnly=TRUE)[1]
-#MyData <- read.csv(file="c:/TheDataIWantToReadIn.csv", header=TRUE, sep=",")
+
 file_name = paste('/app/tmp/', sess_token,'.csv', sep='')
+
 ratings = read.csv(file = file_name, header=FALSE, sep=',', colClasses=c("character", "numeric"))
-mean(ratings[,2])
+
+
+training.data <- read.csv("vendor/norm_mturk_ratings.csv", sep=",", header=TRUE)
+training.data <- training.data[,-1]
+
+# normalize the data
+center_rowmeans <- function(x) {
+	xcenter = rowMeans(x, na.rm=TRUE)
+	x-rep(xcenter,times=ncol(x))
+}
+ratings.norm <- center_rowmeans(ratings)
+
+words <- as.matrix(colnames(training.data))
+words <- cbind(words, c(NA))
+words[,2] <- runif(514,0,1)
+words.sorted <- as.matrix(words[order(words[,2]),1])
+
+print(words.sorted[1:60])
