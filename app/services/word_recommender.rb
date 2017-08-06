edaac6282d80dea3a71bd85503cb3d81978bac68
Richard Niles <richkniles@Richards-MacBook-Pro-3.local>
2017-08-06 13:47:42 -0700

    Add new service in git!

---
+++ app/services/word_recommender.rb
@@ -0,0 +1,31 @@
+class WordRecommender
+  def initialize(ratings)
+    @ratings = ratings
+  end
+
+  def recommended_words
+    write_ratings_to_temp_file
+    result = `Rscript #{Rails.root}/bin/rscript/threatwords.r #{Rails.root} #{file_name}`
+    parse_result(result)
+  end
+
+  private
+
+  def parse_result(r_script_output)
+    r_script_output.gsub(/\[\d+\] /, '').delete('"').split(/[ \n]+/).map(&:strip).select(&:present?)
+  end
+
+  def sess_token
+    @sess_token ||= SecureRandom.urlsafe_base64
+  end
+
+  def file_name
+    @file_name ||= Rails.root.join('tmp', "#{sess_token}.csv")
+  end
+
+  def write_ratings_to_temp_file
+    CSV.open(file_name, 'w') do |csv|
+      @ratings.each { |word, rating| csv << [word, rating] }
+    end
+  end
+end
