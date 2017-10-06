0146a4f89309bdfee345fb3851fb6e03604377e8
Richard Niles <richkniles@Richards-MacBook-Pro-3.local>
2017-10-05 20:49:50 -0700

    Add num_to_recomment param

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -5,7 +5,8 @@ class ApiController < ActionController::Base
     words_with_values = ApplicationController::WORDS.select { |word| params[word].present? }
     @ratings = params.slice(*words_with_values)
     mode = params[:location] == 'local' ? 1 : 2
-    @result = WordRecommender.new(@ratings, mode).recommended_words
+    num_to_recommend = (params[:num_to_recommend] || '60').to_i
+    @result = WordRecommender.new(@ratings, mode, num_to_recommend).recommended_words
     render json: @result
   end
 end
